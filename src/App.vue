107ecd64787ee3d1350e46206cf51319235f6f70
LoganChen <2053536067@qq.com>
2024-07-02 15:31:29 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>hello vue3</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '这是一段测试'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+const app = createApp(App)
+app.mount('#app')
+
+const a = 1010;
+const fun = () => 
+  
+{
+  console.log('这是没有格式化的代码');
+}
